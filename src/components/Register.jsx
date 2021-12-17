f72287f8dae50be557a0b95def20b9822ce13040
Benjamin Kandaris <benjaminkandaris@yahoo.com>
2021-12-17 12:12:02 -0500

    added validation to register page

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -13,32 +13,67 @@ const Register = () => {
   const emailRef = useRef();
   const navigate = useNavigate();
   const [errorHandler, setErrorHandler] = useState(false);
-  console.log(errorHandler);
+  const [userNameErr, setUserNameError] = useState({});
+  const [emailErr, setEmailError] = useState({});
+  const [passwordErr, setPasswordError] = useState({});
+
+  const handleValidation = () => {
+    const userNameErr = {};
+    const emailErr = {};
+    const passwordErr = {};
+    let isValid = true;
+
+    if (usernameRef.current.value.trim().length < 5) {
+      userNameErr.user_nameInvalid = 'Please enter a valid username.';
+      isValid = false;
+    }
+
+    if (!emailRef.current.value.includes('@')) {
+      emailErr.user_emailInvalid = 'Please enter a valid e-mail.';
+      isValid = false;
+    }
+
+    if (passwordRef.current.value.trim().length < 5) {
+      passwordErr.messageInvalid = 'Please enter a longer password.';
+      isValid = false;
+    }
+
+    setUserNameError(userNameErr);
+    setEmailError(emailErr);
+    setPasswordError(passwordErr);
+
+    return isValid;
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
     const user = {
       username: usernameRef.current.value,
       password: passwordRef.current.value,
       email: emailRef.current.value,
     };
-
-    axios
-      .post('https://discer.herokuapp.com/api/user/register', user)
-      .then((res) => {
-        dispatch(
-          registerUser({
-            username: res.data.username,
-            _id: res.data._id,
-            email: res.data.email,
-          })
-        );
-        navigate(`/finishprofile`);
-      })
-      .catch((err) => {
-        setErrorHandler(true);
-      });
+    const isValid = handleValidation();
+    if (isValid) {
+      axios
+        .post('https://discer.herokuapp.com/api/user/register', user)
+        .then((res) => {
+          dispatch(
+            registerUser({
+              username: res.data.username,
+              _id: res.data._id,
+              email: res.data.email,
+            })
+          );
+          navigate(`/finishprofile`);
+        })
+        .catch((err) => {
+          setErrorHandler(true);
+        });
+    }
   };
+
+  console.log('usernameRef', usernameRef);
 
   return (
     <div className='login-wrapper'>
@@ -54,20 +89,20 @@ const Register = () => {
             </div>
             <form onSubmit={handleSubmit}>
               <label>Username</label>
-              <input
-                type='text'
-                placeholder='Username'
-                ref={usernameRef}></input>
+              <input type='text' placeholder='Username' ref={usernameRef} />
+              {Object.keys(userNameErr).map((key) => {
+                return <p style={{ color: 'red' }}>{userNameErr[key]}</p>;
+              })}
               <label>E-mail</label>
-              <input
-                type='email'
-                placeholder='E-mail address'
-                ref={emailRef}></input>
+              <input type='email' placeholder='E-mail address' ref={emailRef} />
+              {Object.keys(emailErr).map((key) => {
+                return <p style={{ color: 'red' }}>{emailErr[key]}</p>;
+              })}
               <label>Password</label>
-              <input
-                type='password'
-                placeholder='Password'
-                ref={passwordRef}></input>
+              <input type='password' placeholder='Password' ref={passwordRef} />
+              {Object.keys(passwordErr).map((key) => {
+                return <p style={{ color: 'red' }}>{passwordErr[key]}</p>;
+              })}
               <button>Register</button>
             </form>
           </div>
